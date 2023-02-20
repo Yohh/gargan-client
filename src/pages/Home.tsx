243cb4b68630c0a5a23cb85b53e92b8a63b74de1
Yohh <durandyohan@zaclys.net>
2023-02-20 18:22:01 +0100

    change favicon, add logo svg component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import FriendsIcon from "../components/svg/FriendsIcon";
+import GarganLogo from "../components/svg/GarganLogo";
 import UserIcon from "../components/svg/UserIcon";
 
 export const Home = () => (
-  <div>
+  <div className="flex justify-around p-2">
+    <GarganLogo />
     <UserIcon />
     <FriendsIcon />
   </div>
